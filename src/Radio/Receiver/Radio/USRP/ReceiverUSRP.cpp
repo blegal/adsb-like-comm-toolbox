8b2734e247f838a32a05382e31a316c0f4d602ba
blegal <bertrand.legal@ims-bordeaux.fr>
2020-12-10 17:27:41 +0100

    USRP update

--- src/Radio/Receiver/Radio/USRP/ReceiverUSRP.cpp
+++ src/Radio/Receiver/Radio/USRP/ReceiverUSRP.cpp
@@ -9,7 +9,7 @@ ReceiverUSRP::ReceiverUSRP(float s_fc, float s_fe) : Receiver(s_fc, s_fe)
 
 ReceiverUSRP::~ReceiverUSRP()
 {
-
+    usrp.reset();
 }
 
 void ReceiverUSRP::initialize(){
@@ -20,7 +20,9 @@ void ReceiverUSRP::initialize(){
 
 	usrp->set_rx_rate(fe);                         // Set de la fréquence d'échantillonnage
 	usrp->set_rx_freq(fc);                         // Set de la fréquence porteuse
-	//usrp->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("AB:0"));
+    usrp->set_rx_gain(20.0);
+
+    //usrp->set_rx_subdev_spec(uhd::usrp::subdev_spec_t("AB:0"));
 	usrp->set_rx_antenna("RX2");
 
 	uhd::stream_args_t
@@ -28,7 +30,6 @@ void ReceiverUSRP::initialize(){
 	rx_stream = usrp->get_rx_stream(stream_args); // Pointeur sur les data reçues
 
 	cout << " " << string(50, '-') << endl;
-	usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
 
 	cout << " Sampling Rate set to     : " << usrp->get_rx_rate() << endl;
 	cout << " Central Frequency set to : " << usrp->get_rx_freq() << endl;
@@ -39,26 +40,41 @@ void ReceiverUSRP::initialize(){
 
 void ReceiverUSRP::reception(vector<complex<float> >& buffer)
 {
-    uhd::rx_metadata_t md;                     // Des metadata
-    int num_rx_samps = 0;                     // Nombre d'echantillons reçus
+//    std::cout << "[ READING USRP SAMPLES ]" << std::endl;
 
-    while (num_rx_samps < buffer.size())
-        num_rx_samps += rx_stream->recv(&buffer.at(num_rx_samps), N - num_rx_samps, md);
+    uhd::rx_metadata_t md;                     // Des metadata
+
+    uint32_t num_rx_samps = 0;                     // Nombre d'echantillons reçus
+    uint32_t nSamples     = buffer.size();
+    while (num_rx_samps < nSamples )
+    {
+        num_rx_samps += rx_stream->recv(&buffer.at(num_rx_samps), nSamples - num_rx_samps, md);
+    }
+    for(int i = 0; i < nSamples; i += 1)
+    {
+        buffer[i] = buffer[i] * 127.0f;
+    }
+    //for(int i = 0; i < nSamples; i += 1)
+    //{
+    //    cout << i << " - " << buffer[i] << std::endl;
+    //}
+//  std::cout << "[ GOING BACK TO WORK ]" << std::endl;
 }
 
 void ReceiverUSRP::reset()
 {
-    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
-	rx_stream.reset();
 	usrp.reset();
 }
 
 void ReceiverUSRP::start_engine()
 {
-
+    std::cout << "[ STARTING USRP ]" << std::endl;
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
 }
 
 void ReceiverUSRP::stop_engine()
 {
-
+    std::cout << "[ STOPPING USRP ]" << std::endl;
+    usrp->issue_stream_cmd(uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
+    rx_stream.reset();
 }
